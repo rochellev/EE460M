fbe42c66c5383c1928f3202c10f942870569eeb3
Deplexx <nico.cortes@austin.utexas.edu>
2016-11-08 16:22:37 -0600

    Project 6 initial commit. Added skeleton controller to proj6 directory.

---
+++ proj6/lab6.v
@@ -0,0 +1,87 @@
+module top(clk, mode, btns, swtchs, leds, segs, an);
+  input clk;
+  input[1:0] mode;
+  input[1:0] btns;
+  input[7:0] swtchs;
+  output[7:0] leds;
+  output[6:0] segs;
+  output[3:0] an;
+
+  //might need to change some of these from wires to regs
+  wire cs;
+  wire we;
+  wire[6:0] addr;
+  wire[7:0] data_out_mem;
+  wire[7:0] data_out_ctrl;
+  wire[7:0] data_bus;
+
+  //MODIFY THE RIGHT HAND SIDE OF THESE TWO STATEMENTS ONLY
+  assign data_bus = 1; // 1st driver of the data bus -- tri state switches,
+                       // logical function of we and data_out_ctrl
+
+  assign data_bus = 1; // 2nd driver of the data bus -- tri state switches,
+                       // logical function of we and data_out_mem
+
+
+  controller ctrl(clk, cs, we, addr, data_bus, data_out_ctrl, mode,
+    btns, swtchs, leds, segs, an);
+
+  memory mem(clk, cs, we, addr, data_bus, data_out_mem);
+
+  //add any other functions you need
+  //(e.g. debouncing, multiplexing, clock-division, etc)
+
+endmodule
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+module controller(clk, cs, we, address, data_in, data_out, mode, btns, swtchs, leds, segs, an);
+  input clk;
+  output cs;
+  output we;
+  output[6:0] address;
+  input[7:0] data_in;
+  output[7:0] data_out;
+  input[1:0] mode;
+  input[1:0] btns;
+  input[7:0] swtchs;
+  output[7:0] leds;
+  output[6:0] segs;
+  output[3:0] an;
+
+
+  //WRITE THE FUNCTION OF THE CONTROLLER
+
+endmodule
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+module memory(clock, cs, we, address, data_in, data_out);
+  //DO NOT MODIFY THIS MODULE
+  input clock;
+  input cs;
+  input we;
+  input[6:0] address;
+  input[7:0] data_in;
+  output[7:0] data_out;
+
+  reg[7:0] data_out;
+
+  reg[7:0] RAM[0:127];
+
+  always @ (negedge clock)
+  begin
+    if((we == 1) && (cs == 1))
+      RAM[address] <= data_in[7:0];
+
+    data_out <= RAM[address];
+  end
+endmodule

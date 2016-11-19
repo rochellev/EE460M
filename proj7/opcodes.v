56ddbc67dd12617b7fa8301b9966d7815462f3fe
rochellev <rochellevroberts@gmail.com>
2016-11-19 13:11:57 -0600

    opcodes for the new instructions

---
+++ proj7/opcodes.v
@@ -0,0 +1,16 @@
+
+//non-special
+parameter jal 6'b000011;
+parameter lui 6'b001111;
+
+//special
+parameter mult 6'b011000;
+parameter mfhi 6'b010000;
+parameter mflo 6'b010010;
+parameter add8 6'b101101;
+parameter rbit 6'b101111;
+parameter rev  6'b110000;
+parameter sadd 6'b110001;
+parameter ssub 6'b110010;
+
+
